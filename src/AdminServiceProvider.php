0c27f023f06b7b9fcf2d0d719382ff53d8bd8600
Simão Coutinho <simaopedrocoutinho@gmail.com>
2021-05-17 09:27:07 +0100

    adicionada informação da documentação no AdminServiceProvider.php

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -12,7 +12,6 @@ class AdminServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->make('SimaoCoutinho\Admin\Controllers\AdminController');
-        $this->loadViewsFrom(__DIR__.'/views', 'admin');
     }
 
     /**
@@ -20,18 +19,39 @@ class AdminServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadViewsFrom(__DIR__ . '/views', 'admin');
+        //$this->publishes([
+        //        __DIR__.'/../resources/views' => resource_path('views/vendor/courier'),
+        //    ]);
+
+        //$this->loadViewComponentsAs('courier', [
+        //        Alert::class,
+        //        Button::class,
+        //    ]);
+
         // Se tiver migrações
         // return $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 //        $this->publishes([
 //            __DIR__.'/database/migrations' => database_path('migrations'),
 //        ], 'migrations');
 
+        $this->publishes([
+            __DIR__ . '/public/' => public_path('vendor/simao-coutinho'),
+        ], 'public');
+
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/public/' => public_path('vendor/simao-coutinho'),
-            ], 'public');
+//$this->commands([
+//            InstallCommand::class,
+//            NetworkCommand::class,
+//        ]);
         }
 
-        include __DIR__.'/routes/web.php';
+        //$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        include __DIR__ . '/routes/web.php';
+
+        //$this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'courier');
+        //$this->publishes([
+        //        __DIR__.'/../resources/lang' => resource_path('lang/vendor/courier'),
+        //    ]);
     }
 }
