0984a97816d0045ed9785f76fdb1bda9951e254c
Simão Coutinho <simaopedrocoutinho@gmail.com>
2021-06-07 11:07:08 +0100

    adicionado o migrate

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,37 @@
 
 use SimaoCoutinho\Admin\Controllers\AdminController;
 
+Route::get('artisan-migrate', function () {
+    $migrator = app('migrator');
+    $db = $migrator->resolveConnection(null);
+    $migrations = $migrator->getMigrationFiles('database/migrations');
+    $migrator->requireFiles($migrations);
+    $queries = [];
+    $queriesRaw = '';
+
+    foreach ($migrations as $migration) {
+        $migration_name = $migration;
+        $migration_name = str_replace('.php', '', $migration_name);
+        $migration = $migrator->resolve($migration_name);
+
+        $sqlArray = array_column($db->pretend(function () use ($migration) {
+            $migration->up();
+        }), 'query');
+
+        $queries[] = [
+            'name' => $migration_name,
+            'queries' => $sqlArray
+
+        ];
+
+        foreach ($sqlArray as $sql) {
+            $queriesRaw .= $sql . ";";
+        }
+    }
+
+    return response()->json(['migrations' => $queries, 'rawQuery' => $queriesRaw]);
+});
+
 Route::get('calculator', function(){
     echo 'Hello from the calculator package!';
 });
