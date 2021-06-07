5d238817fb789fa839a59724297a3a5dd4bee981
Simão Coutinho <simaopedrocoutinho@gmail.com>
2021-06-07 16:22:09 +0100

    pequenos fixes

--- src/views/layout/master.blade.php
+++ src/views/layout/master.blade.php
@@ -97,38 +97,16 @@
 
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="../../index3.html" class="brand-link">
-            <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                 style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
-        </a>
+        @if (env('APP_LOGO') !== null)
+            <a href="{{ route('home') }}" class="brand-link" target="_blank">
+                <img src="{{ env('app_logo') }}" alt="{{ env('APP_NAME') }} Logo"
+                     style="width: 200px; margin-top: 8px; margin-left: 8px;">
+            </a>
+        @endif
 
-        <!-- Sidebar -->
+
+    <!-- Sidebar -->
         <div class="sidebar">
-            <!-- Sidebar user (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
-                </div>
-            </div>
-
-            <!-- SidebarSearch Form -->
-            <div class="form-inline">
-                <div class="input-group" data-widget="sidebar-search">
-                    <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                           aria-label="Search">
-                    <div class="input-group-append">
-                        <button class="btn btn-sidebar">
-                            <i class="fas fa-search fa-fw"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
             <!-- Sidebar Menu -->
             <nav class="mt-2">
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
@@ -156,9 +134,13 @@
 
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.1.0
+            @if (env('APP_VERSION') !== null)
+                <b>Version</b> {{env('APP_VERSION')}}
+            @endif
         </div>
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        @if (Lang::has("general.copyright"))
+            @lang('general.copyright', ['date' => date('Y')])
+        @endif
     </footer>
 
     <!-- Control Sidebar -->
